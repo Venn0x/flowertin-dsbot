ef2d023b41278bc2500f3babe77a3d10110b257e
Vennox <mihai1209ro@gmail.com>
2021-04-07 17:16:21 +0300

    Create varu.js

---
+++ commands/fun/varu.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'varu',
+  args: false,
+  guildOnly: true,
+  description: 'Cf varu',
+  execute(message, args) {
+    message.channel.send('CEEEE FACI VARUUUUUU');
+  },
+};
